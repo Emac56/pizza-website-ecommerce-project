57a03a3926d83f7a84ef2eec41f72bbcf735fb51
Emac56 <emacvicente8@gmail.com>
2026-06-03 18:46:39 +0800

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,21 @@
-const mysql = require("mysql2");
+const { Pool } = require("pg");
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+const pool = new Pool({
+
+host: process.env.DB_HOST,
+
+port: process.env.DB_PORT,
+
+database: process.env.DB_NAME,
+
+user: process.env.DB_USER,
+
+password: process.env.DB_PASSWORD,
+
+ssl: {
+rejectUnauthorized: false
+}
+
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.log("Database connection failed");
-    console.log(err);
-    return;
-  }
-
-  console.log("MySQL connected");
-});
-
-module.exports = connection;
-
+module.exports = pool;

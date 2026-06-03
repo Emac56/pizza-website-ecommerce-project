14127eb2dd4044dffd6af13aa57a1e66b702745b
Emac56 <emacvicente8@gmail.com>
2026-06-03 19:35:34 +0800

    Update auth.js

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -23,7 +23,7 @@ if (!loginInput || !password) {
 try {
 
   const response = await fetch(
-    "https://golden-crust-api.onrender.com/auth/login",
+    "https://golden-crust-api.onrender.com/api/auth/login",
     {
       method: "POST",
 
@@ -135,7 +135,7 @@ if (password !== confirmPassword) {
 try {
 
   const response = await fetch(
-    "https://golden-crust-api.onrender.com/auth/signup",
+    "https://golden-crust-api.onrender.com/api/auth/signup",
     {
       method: "POST",
 

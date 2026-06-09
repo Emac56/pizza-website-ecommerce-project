72837b309c923f434e81d75ad75b1053ecd31a8f
Emac56 <emacvicente@gmail.com>
2026-06-09 09:14:28 +0800

    Display user orders in order history page

---
+++ frontend/js/order-history.js
@@ -0,0 +1,101 @@
+
+loadOrders();
+
+async function loadOrders() {
+
+  const userId =
+  localStorage.getItem("userId");
+
+  if (!userId) {
+    return;
+  }
+
+  try {
+
+    const response =
+    await fetch(
+      `${API_BASE_URL}/api/orders/user/${userId}`
+    );
+
+    const orders =
+    await response.json();
+
+    const ordersContainer =
+document.querySelector(
+  "#ordersContainer"
+);
+
+ordersContainer.innerHTML = "";
+
+for (const order of orders) {
+
+  ordersContainer.innerHTML += `
+  
+  <div
+    class="bg-white
+           rounded-3xl
+           p-6
+           shadow"
+  >
+
+    <div
+      class="flex
+             justify-between
+             items-center"
+    >
+
+      <div>
+
+        <h2
+          class="text-xl
+                 font-bold"
+        >
+          Order #${order.id}
+        </h2>
+
+        <p
+          class="text-gray-500"
+        >
+          ${new Date(
+            order.created_at
+          ).toLocaleDateString()}
+        </p>
+
+      </div>
+
+      <span
+        class="bg-yellow-100
+               text-yellow-700
+               px-3 py-1
+               rounded-full"
+      >
+        ${order.payment_method.toUpperCase()}
+      </span>
+
+    </div>
+
+    <div
+      class="mt-4"
+    >
+
+      <p>
+        ${order.city}
+      </p>
+
+    </div>
+
+  </div>
+
+  `;
+
+}
+
+  }
+
+  catch (error) {
+
+    console.log(error);
+
+  }
+
+}

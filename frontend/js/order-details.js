f10563c68bf18a5552f21a293f560128c75fefb0
Emac56 <emacvicente@gmail.com>
2026-06-10 16:57:59 +0800

    update order-details.js

--- frontend/js/order-details.js
+++ frontend/js/order-details.js
@@ -1,13 +1,73 @@
 const params =
 new URLSearchParams(
-  window.location.search
+window.location.search
 );
 
 const orderId =
 params.get("id");
 
-console.log(orderId);
+loadOrder();
 
-fetch(
-  `${API_BASE_URL}/api/orders/${orderId}/items`
-)
+async function loadOrder() {
+
+try {
+
+const response =
+await fetch(
+  `${API_BASE_URL}/api/orders/${orderId}`
+);
+
+const order =
+await response.json();
+
+document.querySelector(
+  "#orderDetails"
+).innerHTML = `
+
+  <h1 class="text-3xl font-bold mb-4">
+    Order #${order.id}
+  </h1>
+
+  <div class="space-y-2">
+
+    <p>
+      <strong>Status:</strong>
+      ${order.status}
+    </p>
+
+    <p>
+      <strong>Payment:</strong>
+      ${order.payment_method.toUpperCase()}
+    </p>
+
+    <p>
+      <strong>Name:</strong>
+      ${order.full_name}
+    </p>
+
+    <p>
+      <strong>Phone:</strong>
+      ${order.phone_number}
+    </p>
+
+    <p>
+      <strong>Address:</strong>
+      ${order.street_address}
+    </p>
+
+    <p>
+      <strong>City:</strong>
+      ${order.city}
+    </p>
+
+  </div>
+
+`;
+
+} catch (error) {
+
+console.log(error);
+
+}
+
+}
